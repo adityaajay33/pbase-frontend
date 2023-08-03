e327fa7099605b311b734db48dc2753e8245cbad
adityaajay33 <107756503+adityaajay33@users.noreply.github.com>
2023-08-03 19:36:42 -0400

    updated login and register form

--- src/components/login-form/login-form.jsx
+++ src/components/login-form/login-form.jsx
@@ -1,14 +1,32 @@
 import React from 'react'
 import "./login-form.css"
+import Form from 'react-bootstrap/Form'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
 
 export default function LoginForm() {
   return (
     <div className="loginPage">
       <div className="boxBlack">
         <div className="titleLog">Login</div>
-        <div className="eMailLogin"><input type="email" placeholder='E-Mail' className="inputField" /></div>
-        <div className="passwordLogin"><input type="password" placeholder="Password" className="inputField"></input></div>
-        <div className="loginButton"><button class="button-9">Login</button> </div>
+        <Form className="formReg">
+            <Row>
+              <Form.Group className="mb-3" controlId="formGroupEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter E-mail" />
+              </Form.Group>
+            </Row>
+            <Row>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+            </Row>
+            
+            <button variant="primary" type="submit" className="signupButton">
+              Login
+            </button>
+          </Form>
       </div>
     </div>
   )
--- src/components/register-form/registerForm.jsx
+++ src/components/register-form/registerForm.jsx
@@ -1,16 +1,53 @@
 import React from 'react'
 import "./registerForm.css"
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
 
 export default function RegisterForm() {
   return (
     <div className="registerPage">
       <div className="boxWhite">
         <div className="titleReg">Register</div>
-        <div className="firstNameReg"><input type="text" placeholder='First Name' className="inputField" /></div>
-        <div className="lastNameReg"><input type="text" placeholder='Last Name' className="inputField" /></div>
-        <div className="eMail"><input type="email" placeholder='E-Mail' className="inputField" /></div>
-        <div className="institution"><input type="institution" placeholder="Institution" className="inputField" /></div>
-        <div className="signupButton"><button class="button-11" >Sign Up</button> </div>
+        <Form className="formReg">
+            <Row>
+              <Col>
+                <Form.Group className="mb-3">
+                  <Form.Label>First Name</Form.Label>
+                  <Form.Control type="text" placeholder="First Name" />
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group className="mb-3">
+                  <Form.Label>Last Name</Form.Label>
+                  <Form.Control type="text" placeholder="Last Name" />
+                </Form.Group>
+              </Col>
+            
+            </Row>
+            <Row>
+              <Form.Group className="mb-3">
+                <Form.Label>Institution</Form.Label>
+                <Form.Control type="text" placeholder="Institution" />
+              </Form.Group>
+            </Row>
+            <Row>
+              <Form.Group className="mb-3" controlId="formGroupEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter E-mail" />
+              </Form.Group>
+            </Row>
+            <Row>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+            </Row>
+          <button variant="primary" type="submit" className="signupButton">
+            Sign Up
+          </button>
+        </Form>
       </div>
     </div>
   )
