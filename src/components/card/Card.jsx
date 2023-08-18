ba97145db9a06111c5b6fd8f3ca3bd53dd2ed59f
adityaajay33 <107756503+adityaajay33@users.noreply.github.com>
2023-08-17 21:40:59 -0400

    added more functionality to cards component

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
-import "Card.css"
+import "./Card.css"
+import "../../assets/placeholder_img.png"
 
-export default function UserCards() {
+export default function UserCards({firstName, lastName, institution}) {
   return (
     <div className="outerDiv">
-
+        <img src="../../assets/placeholder_img.png" className="pictureUser" />
+        <div className="userName">
+            {firstName} {lastName}
+        </div>
+        <div className="userUniversity">
+            {institution}
+        </div>
     </div>
   )
 }
--- src/components/feed/feed.jsx
+++ src/components/feed/feed.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./feed.css"
+import UserCards from '../card/Card'
+import axios from "axios"
+
 
 export default function Feed() {
+
+  const [cards, setCards] = useState([]);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [institution, setInstitution] = useState("");
+
+  useEffect(() => {
+     const configuration ={
+      method: "get",
+      url: 'http://localhost:5000/api/portfolios/'
+     }
+
+     axios(configuration).then((result) =>{
+      setFirstName(result.data.userLink.firstName);
+      setLastName(result.data.userLink.lastName);
+      setInstitution(result.data.userLink.institution);
+     }).catch((error) => {
+      console.error("Error fetching user data:", error);
+    });
+  }, []);
+
   return (
     <div className='feedPortfolios'>
+      {cards.map(card => (
+        <UserCards firstName={firstName} lastName="Doe" institution="TMU" />
+        ))}
       
     </div>
   )
